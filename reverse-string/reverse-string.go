068b1941152d03f6c7f63fe13c334decb01929c6
Fernando Rocha <rocha@slb.com>
2018-10-31 21:50:57 -0500

    recerse-string

---
+++ reverse-string/reverse-string.go
@@ -0,0 +1,10 @@
+package reverse
+
+//String should
+func String(word string) string {
+	r := []rune(word)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
